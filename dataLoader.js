1457a71d01481f958e06c1b002fd8cac229fcf26
Paul M. Christian <paulmchristian@gmail.com>
2019-03-29 23:02:50 -0500

    import csv data; optimize processing time

--- dataLoader.js
+++ dataLoader.js
@@ -1,9 +1,10 @@
 const _ = require('lodash');
+const csv = require('csvtojson');
 const fs = require('fs');
 const moment = require('moment');
 
-const startDate = moment('2016-7-1', 'YYYY-MM-DD');
-const endDate = moment();
+const startDate = moment('1880-1-1', 'YYYY-MM-DD');
+const endDate = moment('2018-1-1', 'YYYY-MM-DD');
 const intervalMillis = 100;
 const programDuration = moment.duration(3, 'minutes');
 const tickCount = programDuration.asMilliseconds() / intervalMillis;
@@ -18,93 +19,120 @@ let weeklyDatasource1 = _.range(1000)
   .reverse()
   .map(n => ([moment().subtract(n, 'weeks'), _.random(10, 100, true)]));
 
-const dataSources = [
-  {
-    address: '/daily1',
-    dateFormat: '',
-    dataSource: dailyDatasource1,
-    startIndex: 0,
-    type: 'f',
-  },
-  {
-    address: '/weekly1',
-    dateFormat: '',
-    dataSource: weeklyDatasource1,
-    startIndex: 0,
-    type: 'f',
-  },
-];
+
+const parseCsvFile = filepath =>
+  csv({
+    noheader: true,
+    output: "csv",
+  })
+    .fromFile(filepath);
 
 
-const ticks = _.range(tickCount)
-  .map(tick => {
-    const time = startDate.clone().add(scaledInterval.asSeconds() * tick, 'seconds');
-    const nextTime = startDate.clone().add(scaledInterval.asSeconds() * (tick + 1), 'seconds');
+const processData = async () => {
+  const dataSources = [
+    {
+      address: '/daily1',
+      dateFormat: '',
+      dataSource: dailyDatasource1,
+      startIndex: 0,
+      type: 'f',
+    },
+    {
+      address: '/weekly1',
+      dateFormat: '',
+      dataSource: weeklyDatasource1,
+      startIndex: 0,
+      type: 'f',
+    },
+    {
+      address: '/co2',
+      dateFormat: 'DD-MMM-YYYY hh:mm:ss',
+      dataSource: await parseCsvFile('data/co2.csv'),
+      startIndex: 0,
+      type: 'f',
+    },
+    {
+      address: '/temps',
+      dateFormat: 'YYYY',
+      dataSource: await parseCsvFile('data/temps.csv'),
+      startIndex: 0,
+      type: 'f',
+    },
+  ];
 
-    return ([
-      ...[ // Simulation Time
-        ['/time/formatted', 'dddd, MMMM Do YYYY, h:mm:ss a'],
-        ['/time/month', 'MMM'],
-        ['/time/day', 'DD'],
-        ['/time/year', 'YYYY'],
-        ['/time/hourminute', 'HH:mm'],
-      ].map(([address, formatter]) => ({
-        address,
-        args: [{
-          type: 's',
-          value: time.format(formatter),
-        }]
-      })),
+  const ticks = _.range(tickCount)
+    .map(tick => {
+      const time = startDate.clone().add(scaledInterval.asSeconds() * tick, 'seconds');
+      const nextTime = startDate.clone().add(scaledInterval.asSeconds() * (tick + 1), 'seconds');
 
-      ..._.compact(
-        dataSources.map((dataSourceConfig, dataSourceKey) => {
-          const {
-            address,
-            dateFormat,
-            dataSource,
-            startIndex,
-            type,
-          } = dataSourceConfig;
+      return ([
+        ...[ // Simulation Time
+          ['/time/formatted', 'dddd, MMMM Do YYYY, h:mm:ss a'],
+          ['/time/month', 'MMM'],
+          ['/time/day', 'DD'],
+          ['/time/year', 'YYYY'],
+          ['/time/hourminute', 'HH:mm'],
+        ].map(([address, formatter]) => ({
+          address,
+          args: [{
+            type: 's',
+            value: time.format(formatter),
+          }]
+        })),
 
-          const getTimeFromDatum = ([timestamp]) => moment(timestamp, dateFormat);
-
-          const firstSampleTime = getTimeFromDatum(_.first(dataSource));
-          const lastSampleTime = getTimeFromDatum(_.last(dataSource));
-
-          if (firstSampleTime.isAfter(nextTime) || lastSampleTime.isBefore(time)) {
-            return false;
-          } else {
-
-            const index = _.findIndex(
+        ..._.compact(
+          dataSources.map((dataSourceConfig, dataSourceKey) => {
+            const {
+              address,
+              dateFormat,
               dataSource,
-              (datum) => {
-                const sampleTime = getTimeFromDatum(datum);
-                return time.isBefore(sampleTime) && nextTime.isAfter(sampleTime);
-              },
               startIndex,
-            );
+              type,
+            } = dataSourceConfig;
 
-            if (index === -1) {
+            const getTimeFromDatum = ([timestamp]) => moment(timestamp, dateFormat);
+
+            const firstSampleTime = getTimeFromDatum(_.first(dataSource));
+            const lastSampleTime = getTimeFromDatum(_.last(dataSource));
+
+            if (firstSampleTime.isAfter(nextTime) || lastSampleTime.isBefore(time)) {
               return false;
             } else {
-              dataSources[dataSourceKey].currIndex = index;
-              return {
-                address,
-                args: [{ type, value: dataSource[index][1] }]
+
+              const index = _.findIndex(
+                dataSource,
+                (datum) => {
+                  const sampleTime = getTimeFromDatum(datum);
+                  return time.isSameOrBefore(sampleTime) && nextTime.isAfter(sampleTime);
+                },
+                startIndex,
+              );
+
+              if (index === -1) {
+                return false;
+              } else {
+                dataSources[dataSourceKey].startIndex = index;
+                return {
+                  address,
+                  args: [{ type, value: dataSource[index][1] }]
+                }
               }
             }
-          }
-        })
-      ),
+          })
+        ),
 
-    ]);
-  });
+      ]);
+    });
 
 
-console.log('writing file...');
-fs.writeFileSync('./broadcastData.json', JSON.stringify({
-  intervalMillis,
-  ticks,
-}));
+  console.log('writing file...');
+  fs.writeFileSync('./broadcastData.json', JSON.stringify({
+    intervalMillis,
+    ticks,
+  }));
 
-process.exit()
+  process.exit()
+};
+
+
+processData();
